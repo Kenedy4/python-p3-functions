26cff2aad9a1a823bdd5e88cdd9f33b535504b9f
kenedy4 <kennedymusyoki8@gmail.com>
2024-08-29 07:21:28 +0300

    The Adding Test passed

--- lib/functions.py
+++ lib/functions.py
@@ -11,7 +11,8 @@ def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
 greet_with_default()
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(45,55)
 
 def halve(number):
-    pass
+    
